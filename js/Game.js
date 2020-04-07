54875d9d656eff6f8741df06b1af23552b1920d0
Robin Westerhoff <rwesterhoff@me.com>
2020-04-07 14:08:37 +0200

    Refactor for sumission.

--- js/Game.js
+++ js/Game.js
@@ -10,91 +10,71 @@ class Game {
         this.overlay = document.getElementById('overlay');
     }
 
-    /*
-    Hides the start screen overlay, calls the getRandomPhrase() method, 
-    and sets the activePhrase property with the chosen phrase. It also 
-    adds that phrase to the board by calling the addPhraseToDisplay() 
-    method on the active Phrase object.
-    */
     startGame() {
+        // Hide initial overlay
         this.overlay.style.display = 'none';
+        // Call a random phrase
         this.activePhrase = new Phrase(this.getRandomPhrase());
+        // Add the phrase to the board
         this.activePhrase.addPhraseToDisplay();
     }
 
-    /*
-    This method randomly retrieves one of the phrases stored in the phrases array and 
-    returns it.
-    */
     getRandomPhrase() {
         let count = Math.floor(Math.random() * this.phrases.length);
+        // Retrieve one of the phrases provided
         return this.phrases[count];
     }
 
-    /*
-    This method controls most of the game logic. It checks to see if the 
-    button clicked by the player matches a letter in the phrase, and then 
-    directs the game based on a correct or incorrect guess. This method should:
-    - Disable the selected letter’s onscreen keyboard button.
-    - If the phrase does not include the guessed letter, add the wrong CSS class to the selected letter's keyboard button and call the removeLife() method.
-    - If the phrase includes the guessed letter, add the chosen CSS class to the selected letter's keyboard button, call the showMatchedLetter() method on the phrase, and then call the checkForWin() method. If the player has won the game, also call the gameOver() method.
-    */
     handleInteraction(letter) {
-
+        // Check all keys of onscreen keyboard and see if the key pressed matches and apply styling
         function styleKey(styling) {
             const keys = document.querySelectorAll('.key');
+
             keys.forEach(key => {
-                if (key.textContent == letter) key.classList.add(styling)
+                if (key.textContent == letter) {
+                    key.classList.add(styling)
+                    key.setAttribute('disabled', 'disabled');
+                }
             })
         };
 
+        // Check if the letter is in the active phrase
         if (this.activePhrase.checkLetter(letter)) {
             styleKey('chosen');
+            // And show the letter on the onscreen board
             this.activePhrase.showMatchedLetter(letter);
+            // And see if the game has ended by winning
             if (this.checkForWin()) this.gameOver('win', "You've won!");
+        // With no match in the active phrase
         } else {
             styleKey('wrong');
+            // Remove 1 life
             this.removeLife();
         }
     }
 
-    /*
-    This method removes a life from the scoreboard, by replacing one of the liveHeart.png 
-    images with a lostHeart.png image (found in the images folder) and increments the 
-    missed property. If the player has five missed guesses (i.e they're out of lives), 
-    then end the game by calling the gameOver() method
-    */
     removeLife() {
-        const tries = document.querySelectorAll('#scoreboard .tries img'),
-            increaseMissed = () => {
-                this.missed += 1
-            };
-        let counter = tries.length - (this.missed + 1);
+        const hearts = document.querySelectorAll('#scoreboard .tries img');
+        let count = (hearts.length - 1) - this.missed;
 
-        tries[counter].setAttribute('src', 'images/lostHeart.png');
+        // Set the most right positioned life heart to a lost heart
+        hearts[count].setAttribute('src', 'images/lostHeart.png');
 
-        if (this.missed <= tries.length - 2) {
-            increaseMissed();
+        // Check if there are life hearts left otherwise call for a gameover
+        if (this.missed <= hearts.length - 2) {
+            this.missed += 1;
         } else {
             this.gameOver('lose', "Sorry, you've lost.");
         }
     }
 
-    /*
-    This method checks to see if the player has revealed all of the letters 
-    in the active phrase.
-    */
     checkForWin() {
+        // Check if no letter is hidden
         return document.querySelectorAll('.hide.letter').length === 0;
     }
 
-    /*
-    This method displays the original start screen overlay, and depending on the 
-    outcome of the game, updates the overlay h1 element with a friendly win or loss 
-    message, and replaces the overlay’s start CSS class with either the win 
-    or lose CSS class.
-    */
     gameOver(styling, message) {
+        // Set styling and message of the game over
         this.overlay.className = styling;
         this.overlay.style.display = 'inherit';
         this.overlay.firstElementChild.textContent = message;
--- js/Phrase.js
+++ js/Phrase.js
@@ -7,48 +7,35 @@ class Phrase {
         this.phrase = phrase.toLowerCase();
     }
 
-    /*
-    This adds letter placeholders to the display when the game starts. 
-    Each letter is presented by an empty box, one li element for each letter. 
-    See the example_phrase_html.txt file for an example of what the rendered 
-    HTML for a phrase should look like when the game starts, including any id 
-    or class attributes needed. When the player correctly guesses a letter, 
-    the empty box is replaced with the matched letter (see the showMatchedLetter() 
-    method below). Make sure the phrase displayed on the screen uses the letter 
-    CSS class for letters and the space CSS class for spaces.
-    */
     addPhraseToDisplay() {
+        // Split up the phrase in all seperate characters
         this.phrase.split('').forEach(char => {
             const li = document.createElement('li'),
                 regChar = /[a-z]/g,
                 regSpace = /\s/g;
 
+            // Occupy a list item with the character
             li.textContent = char;
 
+            // And check if it's a normal character or a space and apply the right styling
             if (char.match(regChar)) {
                 li.className = `hide letter ${char}`;
             } else if (char.match(regSpace)) {
                 li.className = 'space';
             }
 
+            // Occupy the board with all letters
             document.querySelector('#phrase ul').appendChild(li);
         })
     }
 
-    /*
- 	Checks to see if the letter selected by the player matches a letter in the phrase.
-	*/
     checkLetter(letter) {
+        // Check if the letter provided matches the phrase
         return this.phrase.match(letter);
     }
 
-    /*
-    Reveals the letter(s) on the board that matches the player's selection. 
-    To reveal the matching letter(s), select all of the letter DOM elements that
-    have a CSS class name that matches the selected letter and replace each
-    selected element's hide CSS class with the show CSS class.
-    */
     showMatchedLetter(letter) {
+        // Get the hidden letters on the board and replace with a class to reveal them
         document.querySelectorAll(`.hide.letter.${letter}`).forEach(hiddenLetter => {
             hiddenLetter.className = `show letter ${letter}`;
         })
